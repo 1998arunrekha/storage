7b106a94a6b0517528e5a5a360f8de65e1c1c4c5
1998arunrekha <arunar16061998@gmail.com>
2022-09-06 18:46:37 +0530

    six commit

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "str01" {
-  name     = "st01"
-  location = "West Europe"
+resource "azurerm_resource_group" "sa" {
+  name     = "sa01"
+  location = "east us"
 }
 
-resource "azurerm_storage_account" "testdemostr02" {
-  name                     = "productionstorage"
-  resource_group_name      = azurerm_resource_group.str01.name
-  location                 = azurerm_resource_group.str01.location
+resource "azurerm_storage_account" "sa11" {
+  name                     = "testsa"
+  resource_group_name      = azurerm_resource_group.sa.name
+  location                 = azurerm_resource_group.sa.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
