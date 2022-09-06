03134befda64f71f743a15338c7dc6d89020d8c2
1998arunrekha <arunar16061998@gmail.com>
2022-09-06 18:38:29 +0530

    fifth commit

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "str01" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "demostr01" {
-  name                     = "examplestorage"
+resource "azurerm_storage_account" "testdemostr02" {
+  name                     = "productionstorage"
   resource_group_name      = azurerm_resource_group.str01.name
   location                 = azurerm_resource_group.str01.location
   account_tier             = "Standard"
