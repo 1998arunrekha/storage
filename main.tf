f835ffba0c4541734094fc9bf2974b1994d5a20d
1998arunrekha <arunar16061998@gmail.com>
2022-09-06 18:14:50 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "str01" {
+  name     = "st01"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "demostr01" {
+  name                     = "demostorage"
+  resource_group_name      = azurerm_resource_group.str01.name
+  location                 = azurerm_resource_group.str01.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "production"
+  }
+}
